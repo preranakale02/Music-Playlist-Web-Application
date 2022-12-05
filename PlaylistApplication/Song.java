7ad2f7b5e1b3dcf82fbe1ef7e0184f0b4a0eb7aa
preranakale02 <preranakale02@gmail.com>
2022-12-05 23:53:02 +0530

    Music Playlist Web Application

---
+++ PlaylistApplication/Song.java
@@ -0,0 +1,36 @@
+
+package PlaylistApplication;
+
+public class Song {
+    private String title;
+    private double duration;
+    
+    public Song(String title,double duration)
+    {
+        this.title=title;
+        this.duration=duration;
+    }
+    public Song()
+    {
+        
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public double getDuration()
+    {
+        return duration;
+    }
+    @Override
+    public String toString()
+    {
+//        return "Song{" +
+//                "title""+ title"
+//                }
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
